91406e2025f39b60498d658e4d1ba1d9cd5510d4
Mark Canlas <mark.canlas@bamtechmedia.com>
2018-07-20 21:05:04 -0400

    enabled comparable bags

--- src/main/scala/com/htmlism/Bag.scala
+++ src/main/scala/com/htmlism/Bag.scala
@@ -6,11 +6,22 @@ object Bag {
   def apply[A](x: A*) =
     new Bag(x)
 
-  /**
-   * Also provides `Equal` over `Bag[A]`.
-   */
-  implicit def order[A](implicit set: Order[List[A]]): Order[Bag[A]] =
-    set.contramap(b => b.xs.toList)
+  implicit def equal[A](implicit eq: Equal[Map[A, Int]]): Equal[Bag[A]] =
+    eq.contramap(countMap)
+
+  def countMap[A](b: Bag[A]): Map[A, Int] =
+    b.xs.foldLeft(Map.empty[A, Int]) { (acc, a) =>
+      if (acc.contains(a))
+        acc.updated(a, acc(a) + 1)
+      else
+        acc.updated(a, 1)
+    }
 }
 
-class Bag[A](protected val xs: Seq[A])
+class Bag[A](protected val xs: Seq[A]) {
+  /**
+   * For console output only.
+   */
+  override def toString: String =
+    "Bag(" + xs.mkString(", ") + ")"
+}
--- src/main/scala/com/htmlism/Ingredient.scala
+++ src/main/scala/com/htmlism/Ingredient.scala
@@ -1,5 +1,8 @@
 package com.htmlism
 
+import scalaz._
+import scalaz.Scalaz.intInstance
+
 object Ingredient {
   val all: Seq[Ingredient] =
     Seq(
@@ -13,26 +16,37 @@ object Ingredient {
       BalmMushroom,
       RainbowMatter,
       MysticalShell)
+
+  // for some reason, providing `Order[Ingredient]` is not enough to power `TinyMushroom === TinyMushroom`
+  // but this does work `(TinyMushroom: Ingredient) === TinyMushroom`
+  // this provides ordering at every inferred level of the hierarchy, not just `Ingredient`
+  implicit def order[A <: Ingredient]: Order[A] =
+    Order.orderBy(_.order)
 }
 
 sealed trait Ingredient {
   def quality: Int
+
+  def order: Int
 }
 
 case object RainbowMatter extends Ingredient {
   val quality = 3
+  val order = 9
 }
 
 case object MysticalShell extends Ingredient {
   val quality = 4
+  val order = 10
 }
 
 abstract class CommonIngredient(
-  val color: Color,
-  val hardness: Hardness,
-  val size: Size,
-  val kind: Kind) extends Ingredient {
-  def quality =
+    val order: Int,
+    val color: Color,
+    val hardness: Hardness,
+    val size: Size,
+    val kind: Kind) extends Ingredient {
+  def quality: Int =
     size match {
       case Small => 1
       case Precious => 2
@@ -63,11 +77,11 @@ case object Mineral  extends Kind
 case object Mushroom extends Kind
 case object Plant    extends Kind
 
-case object TinyMushroom extends CommonIngredient(Red,    Soft, Small,    Mushroom)
-case object BigRoot      extends CommonIngredient(Red,    Soft, Precious, Plant)
-case object BlukBerry    extends CommonIngredient(Blue,   Soft, Small,    Sweet)
-case object IcyRock      extends CommonIngredient(Blue,   Hard, Precious, Mineral)
-case object Apricorn     extends CommonIngredient(Yellow, Hard, Small,    Plant)
-case object Honey        extends CommonIngredient(Yellow, Soft, Precious, Sweet)
-case object Fossil       extends CommonIngredient(Grey,   Hard, Small,    Mineral)
-case object BalmMushroom extends CommonIngredient(Grey,   Soft, Precious, Mushroom)
+case object TinyMushroom extends CommonIngredient(1, Red,    Soft, Small,    Mushroom)
+case object BigRoot      extends CommonIngredient(5, Red,    Soft, Precious, Plant)
+case object BlukBerry    extends CommonIngredient(2, Blue,   Soft, Small,    Sweet)
+case object IcyRock      extends CommonIngredient(6, Blue,   Hard, Precious, Mineral)
+case object Apricorn     extends CommonIngredient(3, Yellow, Hard, Small,    Plant)
+case object Honey        extends CommonIngredient(7, Yellow, Soft, Precious, Sweet)
+case object Fossil       extends CommonIngredient(4, Grey,   Hard, Small,    Mineral)
+case object BalmMushroom extends CommonIngredient(8, Grey,   Soft, Precious, Mushroom)

47d6561e3f80a0081640e3b1df0e93d87eeb349d
Mark Canlas <github@htmlism.com>
2018-07-22 16:41:45 -0400

    added combinator dsl

---
+++ src/main/scala/com/htmlism/combinations/Gen.scala
@@ -0,0 +1,33 @@
+package com.htmlism.combinations
+
+object Gen {
+  def alt[A](xs: A*): Gen[A] =
+    Alt(xs.toList.map(Atom.apply))
+
+  def cat[A](x: A, xs: A*): Gen[A] =
+    xs.foldLeft(Atom(x) : Gen[A])((acc, z) => Product(acc, Atom(z)))
+}
+
+sealed trait Gen[A] {
+  def * (that: Gen[A]): Product[A] =
+    Product(this, that)
+
+  def gen: List[List[A]]
+}
+
+case class Atom[A](x: A) extends Gen[A] {
+  def gen: List[List[A]] =
+    List(List(x))
+}
+case class Alt[A](xs: List[Gen[A]]) extends Gen[A] {
+  def gen: List[List[A]] =
+    xs.flatMap(_.gen)
+}
+
+case class Product[A](left: Gen[A], right: Gen[A]) extends Gen[A] {
+  def gen: List[List[A]] =
+    for {
+      xs <- left.gen
+      ys <- right.gen
+    } yield xs ::: ys
+}
